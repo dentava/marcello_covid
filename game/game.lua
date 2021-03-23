71d665a9fbf99e363df84dab3572cecfda4885c9
ChristianAbbondo <abbondo9000@gmail.com>
2021-03-23 19:09:47 +0100

    aggiunta la mappa

--- game/game.lua
+++ game/game.lua
@@ -1,24 +1,35 @@
 
 local composer = require( "composer" )
-
+local json = require( "json" )
 local scene = composer.newScene()
-
+local tiled = require( "com.ponywolf.ponytiled" )
+local physics=require("physics")
+physics.start();
 -- Torna al menù principale
 local function backHome()
     composer.gotoScene("game.menu_real", { time=800, effect="crossFade" });
 end 
 
+local map
+
 function scene:create( event)
     
     local sceneGroup=self.view
 
-    local txt=display.newText(sceneGroup, "Non esiste alcuno gioco ,ma arriverà credi in Marcello",display.contentCenterX,display.contentCenterY)
+    --local txt=display.newText(sceneGroup, "Non esiste alcuno gioco ,ma arriverà credi in Marcello",display.contentCenterX,display.contentCenterY)
     
     -- Torna al menu
     local buttonbackhome=display.newImageRect(sceneGroup,"image/menu/home_icon.png",70,70);
     buttonbackhome.x = (buttonbackhome.contentWidth/2) 
     buttonbackhome.y = (buttonbackhome.contentHeight/2)
     buttonbackhome:addEventListener("tap", backHome)
+
+
+     --Carico la mappa 
+    local filename ="game/map/test_map.json"
+	local mapData =  json.decodeFile( system.pathForFile( filename, system.ResourceDirectory ) )
+    map = tiled.new( mapData, "game/map")
+	map.xScale, map.yScale = 0.70, 0.70
 end
 
 function scene:show( event )
